f84062d7597d7ff6c55ac85c2bd0fe664d37a6e0
Leo Picado <leo.picado@soundcloud.com>
2021-03-12 16:42:51 +0100

    Prueba

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ module.exports = (app) => {
 			//app.log.info(context.payload);
 			const data = { owner: context.payload.pull_request.repo.owner, repo: context.payload.pull_request.repo.id, pull_number: 3, per_page: 100};
 			app.log.info(data);
-			const files = await context.octokit.pulls.listFiles(data);
-			app.log.info(files);
-			const changedFiles = files.data.map((f) => f.filename)
-			app.log.info(changedFiles);
+			//const files = await context.octokit.pulls.listFiles(data);
+			//app.log.info(files);
+			//const changedFiles = files.data.map((f) => f.filename)
+			//app.log.info(changedFiles);
 			}
 		 );
 
