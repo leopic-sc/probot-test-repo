7206554abfeb7006cec7793f29a73cbb2d620e4a
Leo Picado <leo.picado@soundcloud.com>
2021-03-12 16:09:18 +0100

    More changes

--- index.js
+++ index.js
@@ -8,16 +8,9 @@
 module.exports = (app) => {
   // Your code here
   app.log.info("Yay, the app was loaded!");
-  app.on(
-    ["pull_request.opened", "pull_request.synchronize", "pull_request", "push"],
-    async (context) => {
-      // Creates a deployment on a pull request event
-      // Then sets the deployment status to success
-      // NOTE: this example doesn't actually integrate with a cloud
-      // provider to deploy your app, it just demos the basic API usage.
+  app.log.info(app);
+  app.onAny(async (context) => {
       app.log.info(context.payload);
-
-      // Probot API note: context.repo() => { username: 'hiimbex', repo: 'testing-things' }
     }
   );
 
