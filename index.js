53c26e8cc9595f5bf73569c3b2dd07aabd742f35
Leo Picado <leo.picado@soundcloud.com>
2021-03-12 16:35:57 +0100

    Prueba

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ module.exports = (app) => {
 	// Your code here
 	app.log.info("Yay, the app was loaded!");
 	app.onAny(async (context) => {
-			app.log.info(context.payload.pull_request.number);
-			const files = await octokit.pulls.listFiles({ owner, repo, pull_number, per_page: 100 })
+			app.log.info(context.payload);
+			const files = await context.octokit.pulls.listFiles({ owner, repo, pull_number, per_page: 100 })
 			app.log.info(files);
 			const changedFiles = files.data.map((f) => f.filename)
 			app.log.info(changedFiles);
