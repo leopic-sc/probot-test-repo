d570ce5ea267ea89d0a78c053da32c6d19789bcb
Leo Picado <leo.picado@soundcloud.com>
2021-03-12 16:43:37 +0100

    Prueba

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ module.exports = (app) => {
 	app.log.info("Yay, the app was loaded!");
 	app.onAny(async (context) => {
 			//app.log.info(context.payload);
-			const data = { owner: context.payload.pull_request.repo.owner, repo: context.payload.pull_request.repo.id, pull_number: 3, per_page: 100};
-			app.log.info(data);
+			app.log.info(context.payload.pull_request);
+			//const data = { owner: context.payload.pull_request.repo.owner, repo: context.payload.pull_request.repo.id, pull_number: 3, per_page: 100};
+			//app.log.info(data);
 			//const files = await context.octokit.pulls.listFiles(data);
 			//app.log.info(files);
 			//const changedFiles = files.data.map((f) => f.filename)
